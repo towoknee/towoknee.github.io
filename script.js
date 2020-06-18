ff71f8859b27e26002495b097307062d8846def8
towoknee <49508211+towoknee@users.noreply.github.com>
2020-06-17 20:23:52 -0700

    change to grad

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function counter() {
 	let today = new Date();
 	console.log("today" + today);
 	console.log("endDate" + endDate);
-	let timeLeft = endDate - today;
+	let timeLeft = today - endDate;
 	console.log("timeLeft: " + timeLeft);
 	let sec = timeLeft/1000; 
 	sec = parseInt(sec);
